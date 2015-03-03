6c7646ec327ff185c669b38feeaac7651a75faaa
cgpllx <cgpllx1@qq.com>
2015-03-03 18:01:52 +0800

    Signed-off-by: cgpllx <cgpllx1@qq.com>

--- src/com/konka/dhtsearch/db/models/DhtInfoStateCode.java
+++ src/com/konka/dhtsearch/db/models/DhtInfoStateCode.java
@@ -1,8 +1,8 @@
 package com.konka.dhtsearch.db.models;
 
 public interface DhtInfoStateCode {
-	int NO_DOWNLOAD = 100;
-	int DOWNLOAD_FAILED = 401;
-	int DOWNLOAD_SUCCESS_BUT_PARSING_FAILED = 204;
-	int DOWNLOADSUCCESS_AND_PARSING_SUCCESS = 200;
+	int NO_DOWNLOAD = 100;// 还没有下载
+	int DOWNLOAD_FAILED = 401;// 下载失败
+	int DOWNLOAD_SUCCESS_BUT_PARSING_FAILED = 204;// 下载ok，解析失败
+	int DOWNLOADSUCCESS_AND_PARSING_SUCCESS = 200;// 都ok
 }
