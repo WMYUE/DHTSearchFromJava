8c1c711b55b2eafc61ab73d5cb63281002d2f367
konka <konka@tt-60b40095e5e7>
2015-03-16 19:46:19 +0800

    Signed-off-by: konka <konka@tt-60b40095e5e7>

--- src/org/konkakjb/text/Text3.java
+++ src/org/konkakjb/text/Text3.java
@@ -1,5 +1,19 @@
 package org.konkakjb.text;
 
-public class Text3 {
+import java.io.IOException;
 
+import javax.sound.midi.SysexMessage;
+
+public class Text3 {
+	public static void main(String[] args)  {
+		try {
+			 Process p1 =	Runtime.getRuntime().exec("");
+//			 p1.
+//			 SystemManagerLinux.
+//			 SystemMonitor.
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
